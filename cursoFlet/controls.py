02687e8db998ff4cb1978cc220ec9304f0ee8243
Nuno Batista <nbon.informatica@gmail.com>
2023-10-06 10:10:31 +0100

    pequena alteração

--- cursoFlet/controls.py
+++ cursoFlet/controls.py
@@ -1,8 +1,8 @@
 import flet as ft
 
 def main(page: ft.Page):
-    t=ft.Text(value='Olá Mundo', color='red')
-    page.controls.append(t)
+    lbl_texto=ft.Text(value='Olá Mundo', color='red')
+    page.controls.append(lbl_texto)
     page.update()
 
 ft.app(target=main)
