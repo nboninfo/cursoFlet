f571a9b8d61a973865ef4aa8e693f4bf2b1b773f
Nuno Batista <nbon.informatica@gmail.com>
2023-10-06 12:58:16 +0100

    Update todo.py

--- cursoFlet/todo.py
+++ cursoFlet/todo.py
@@ -8,7 +8,7 @@ def main(page: flet.Page):
 
     nova_tarefa=TextField(hint_text='Que tarefa quer adicionar?', width=300)
     btn_adicionar=ElevatedButton('Adicionar', on_click=add_tarefa)
-
+    
     page.add(Row([nova_tarefa, btn_adicionar]))
 
 flet.app(target=main)
